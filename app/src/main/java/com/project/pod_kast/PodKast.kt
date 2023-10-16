d5d5f48f3460fdfd390ae095ebd5fbbadb7530eb
Nishant Dalal <87112884+nishant0207@users.noreply.github.com>
2023-10-16 19:53:51 +0530

    Delete app directory

--- app/src/main/java/com/project/pod_kast/PodKast.kt
+++
@@ -1,9 +0,0 @@
-package com.project.pod_kast
-
-data class PodKast(
-    val id: String,
-    val title: String,
-    val description: String,
-    val imageUrl: String,
-    val audioUrl: String
-)
