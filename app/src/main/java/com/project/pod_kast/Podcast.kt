58f24be7425d11ccb332593a56d1b73934e66e80
nishant0207 <dalalnishant0207@gmail.com>
2023-10-16 14:58:40 +0530

    First upload for project Pod-Kast

---
+++ app/src/main/java/com/project/pod_kast/Podcast.kt
@@ -0,0 +1,9 @@
+package com.project.pod_kast
+
+data class Podcast(
+    val id: String,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+    val audioUrl: String
+)
